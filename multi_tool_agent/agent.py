8e51c83c5f4723740b26031e3231ac82f9f97c27
savvy <savvyversa@gmail.com>
2025-07-09 16:38:05 +0330

    feat: add google search and code extractor agents as tool

--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -1,7 +1,27 @@
 from google.adk.agents import LlmAgent, Agent
+from google.adk.code_executors import BuiltInCodeExecutor
 from google.adk.models.lite_llm import LiteLlm
+from google.adk.tools import google_search, agent_tool
 
 # --- Example Agent using OpenAI's GPT-4o ---
+
+search_agent = Agent(
+    model=LiteLlm(model="openai/gpt-4o"),
+    name='SearchAgent',
+    instruction="""
+    You're a specialist in Google Search
+    """,
+    tools=[google_search],
+)
+coding_agent = Agent(
+    model=LiteLlm(model="openai/gpt-4o"),
+    name='CodeAgent',
+    instruction="""
+    You're a specialist in Code Execution
+    """,
+    executor=[BuiltInCodeExecutor],
+)
+
 # (Requires OPENAI_API_KEY)
 root_agent = Agent(
     model=LiteLlm(model="openai/gpt-4o"),  # LiteLLM model string format
@@ -11,6 +31,7 @@ root_agent = Agent(
         "Agent to answer questions about the time and weather in a city."
     ),
     instruction=(
-        "You are a helpful agent who can answer user questions about the time and weather in a city."
+        "You are a helpful agent who can answer user questions about the coding using google search"
     ),
+    tools=[agent_tool.AgentTool(agent=search_agent), agent_tool.AgentTool(agent=coding_agent)],
 )
